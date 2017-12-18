6ea16501b56401116ffbc740b2255c45fe4adf39
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-12-18 17:28:29 +0200

    Manually manage memory to improve performance

--- internal/encode.go
+++ internal/encode.go
@@ -5,22 +5,55 @@ import (
 	"encoding/base64"
 	"fmt"
 	"reflect"
+	"sync"
 
 	"github.com/golang/snappy"
 	"github.com/vmihailenco/msgpack"
 )
 
+var buffers = &sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 1024)
+	},
+}
+
 func EncodeArgs(args []interface{}, compress bool) (string, error) {
-	b, err := msgpack.Marshal(args...)
+	b := buffers.Get().([]byte)
+	buf := bytes.NewBuffer(b[:0])
+	enc := msgpack.NewEncoder(buf)
+	err := enc.Encode(args...)
 	if err != nil {
+		buffers.Put(b)
 		return "", err
 	}
+	b = buf.Bytes()
 
+	var b2 []byte
 	if compress {
-		b = snappy.Encode(nil, b)
+		b2 = buffers.Get().([]byte)
+		b2 = snappy.Encode(b2[:0], b)
+	} else {
+		b2 = b
+		b = buffers.Get().([]byte)
 	}
 
-	return base64.StdEncoding.EncodeToString(b), nil
+	b = ensureLen(b, base64.StdEncoding.EncodedLen(len(b2)))
+	base64.StdEncoding.Encode(b, b2)
+	buffers.Put(b2)
+	s := string(b)
+	buffers.Put(b)
+
+	return s, nil
+}
+
+func ensureLen(b []byte, n int) []byte {
+	if n <= cap(b) {
+		b = b[:n]
+		return b
+	}
+	b = b[:cap(b)]
+	b = append(b, make([]byte, n-len(b))...)
+	return b
 }
 
 func DecodeArgs(s string, fnType reflect.Type, decompress bool) ([]reflect.Value, error) {
@@ -28,16 +61,24 @@ func DecodeArgs(s string, fnType reflect.Type, decompress bool) ([]reflect.Value
 		return nil, nil
 	}
 
-	b, err := base64.StdEncoding.DecodeString(s)
+	b := buffers.Get().([]byte)
+	b = ensureLen(b, base64.StdEncoding.DecodedLen(len(s)))
+	n, err := base64.StdEncoding.Decode(b, []byte(s))
 	if err != nil {
+		buffers.Put(b)
 		return nil, err
 	}
+	b = b[:n]
 
 	if decompress {
-		b, err = snappy.Decode(nil, b)
+		b2 := buffers.Get().([]byte)
+		b2, err = snappy.Decode(b2, b)
+		buffers.Put(b)
 		if err != nil {
+			buffers.Put(b2)
 			return nil, err
 		}
+		b = b2
 	}
 
 	buf := bytes.NewBuffer(b)
@@ -46,12 +87,14 @@ func DecodeArgs(s string, fnType reflect.Type, decompress bool) ([]reflect.Value
 	in := make([]reflect.Value, fnType.NumIn())
 	for i := 0; i < fnType.NumIn(); i++ {
 		arg := reflect.New(fnType.In(i)).Elem()
-		if err := dec.DecodeValue(arg); err != nil {
+		err = dec.DecodeValue(arg)
+		if err != nil {
 			err = fmt.Errorf("msgqueue: arg=%d decoding failed: %s", i, err)
-			return nil, err
+			break
 		}
 		in[i] = arg
 	}
 
-	return in, nil
+	buffers.Put(b)
+	return in, err
 }
